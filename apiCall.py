b5ef83e45ece1c819c7b0fb02f55803013c767f1
Anirbaan Ghatak <anirbaanghatak1.ag@gmail.com>
2023-02-16 17:09:45 +0530

    violent crimes

--- apiCall.py
+++ apiCall.py
@@ -1,12 +1,13 @@
 import requests
 import json
+import cars24
 
 # Define the API endpoint URL
 post_url = "https://sm-kyc.scoreme.in/kyc/external/vehicledetails"
 
 # Define the secret key and client ID
-secret_key = "98eafea23ffa16fafbf31d9d2365c940"
-client_id = "d8de75a71334e0d882deb06ba66e3960823239c4f139f0061fd83e86af6e4f2a"
+client_id = "98eafea23ffa16fafbf31d9d2365c940"
+secret_key = "d8de75a71334e0d882deb06ba66e3960823239c4f139f0061fd83e86af6e4f2a"
 vno=input("Enter the vehicle number: ")
 
 # Define the payload for the JSON request
@@ -26,13 +27,19 @@ response = requests.post(post_url, json=payload, headers=headers)
 
 # Print the response content
 print(response.json())
-# refid=response.json()['referenceId']
-get_url="https://sm-kyc.scoreme.in/kyc/external/getkycrequestresponse?referenceId={refid}"
+refid=response.json()['data']['referenceId']
+print(refid)
+get_url=f"https://sm-kyc.scoreme.in/kyc/external/getkycrequestresponse?referenceId={refid}"
 
 res= requests.get(get_url, headers=headers)
 
-# Print the response content
-print(res.json())
+# # Print the response content
+print(res.json()['data']['kycRcVehicleData']['manufacturedDate'])
+print(res.json()['data']['kycRcVehicleData']['makerModel'])
+print(res.json()['data']['kycRcVehicleData']['makerDescription'])
+print(res.json()['data']['kycRcVehicleData']['fuelType'])
+print(res.json()['data']['kycRcVehicleData']['categoryDescription'])
+
 
 #extract make and model and pass to webscraping
 
--- kadu.py
+++ kadu.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import StaleElementReferenceException,NoSuchElementException
 
@@ -23,23 +26,26 @@ def carPrc():
     for i in prices:
         print(i.text)
 
-
-def getC4():
-        try:
-          carPrc()
-        except StaleElementReferenceException:
-            print(f"Search result is stale and could not be located")
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            carPrc()       
-        except NoSuchElementException:
-            print(f"Element missing")
-            driver.refresh()
-
-
-if __name__ == '__main__': 
-    getC4()
-          
-
-
-
+try:
+    carPrc()
+except StaleElementReferenceException:
+    print(f"Search result is stale and could not be located")
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    carPrc()
+        
+except NoSuchElementException:
+    print(f"Element missing")
+    driver.refresh()
     # carPrc(21)
+
+
+
+
+
+
+
+
+    
+# for a in range(10):
+#     prices = driver.find_elements(By.XPATH, f"/html/body/div[1]/div[2]/div[1]/div/div/div[2]/div[4]/div/div/a[1]/div[2]/div[2]/div[1]/div[2]/span")
+#     print(prices[0].text)
