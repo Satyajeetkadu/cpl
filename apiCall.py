ca9e16ba5c0920d31288305e15946dbdcd890a23
KJ173 <krishajoshi1703@gmail.com>
2023-02-24 13:22:52 +0530

    apiCall hopefully fetched

--- apiCall.py
+++ apiCall.py
@@ -1,33 +1,34 @@
 import requests
 import json
 from getCall import get_call
-
-# Define the API endpoint URL
-post_url = "https://sm-kyc-sandbox.scoreme.in/kyc/external/vehicledetails"
-
-# Define the secret key and client ID
-client_id = "4177b870597dced8c482b0dcd7f7b8ec"
-secret_key = "1295024dc9dea537ee8974470e81236c88b1f5eb4f63b61077ee91ee345dbd60"
-vno=input("Enter the vehicle number: ")
-
-# Define the payload for the JSON request
-payload = {"rc_regn_no": vno}
-print(payload)
-# Define the headers for the request
-headers = {
-    "clientId": client_id,
-    "clientSecret": secret_key,
-    "Content-Type": "application/json"    
-}
-
-response = requests.post(post_url, json=payload, headers=headers)
-#extract refid from post response
-
-# Print the response content
-print(response.json())
-refid=response.json()['data']['referenceId']
-print(f"Reference id - {refid}")
-get_call(refid)
+# import cars24
 
 
+def getReg(vno):
+    # Define the API endpoint URL
+    post_url = "https://sm-kyc-sandbox.scoreme.in/kyc/external/vehicledetails"
 
+    # Define the secret key and client ID
+    client_id = "4177b870597dced8c482b0dcd7f7b8ec"
+    secret_key = "1295024dc9dea537ee8974470e81236c88b1f5eb4f63b61077ee91ee345dbd60"
+    
+
+
+    # Define the payload for the JSON request
+    payload = {"rc_regn_no": vno}
+    print(payload)
+    # Define the headers for the request
+    headers = {
+        "clientId": client_id,
+        "clientSecret": secret_key,
+        "Content-Type": "application/json"    
+    }
+
+    response = requests.post(post_url, json=payload, headers=headers)
+    # Print the response content
+    print(response.json())
+    refid=response.json()['data']['referenceId']
+    print(f"Reference id - {refid}")
+    details=get_call(refid)
+
+    return details
