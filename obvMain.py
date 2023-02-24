2617f1015aac42d4a3e7d700afe8a24c47c159cb
KJ173 <krishajoshi1703@gmail.com>
2023-02-24 14:00:54 +0530

    api not integrating

--- obvMain.py
+++ obvMain.py
@@ -1,3 +1,12 @@
 from orangevaluehack import orangebookvalue
-from getCall import get_call
+from apiCall import getReg
 
+def main():
+    vno=input("Enter the vehicle number: ")
+    details=getReg(vno)
+    print(details)
+    orangebookvalue(details)
+
+
+if __name__=="__main__":
+    main()
