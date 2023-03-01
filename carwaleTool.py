86e75417bc3deb139c18c7c4339522c7b14b8fb6
Anirbaan Ghatak <anirbaanghatak1.ag@gmail.com>
2023-03-02 01:23:57 +0530

    car model and version halfway determined

--- carwaleTool.py
+++ carwaleTool.py
@@ -38,7 +38,17 @@ def getPlc(city):
         return "/html/body/div/div[4]/div[2]/div[2]/div[2]/div/div/div[2]/div/div[10]/div/div/img"
     
 
-def selCar(cm, mm, fuel, km):
+def getvalue():
+    cardets=driver.find_element(By.XPATH,f"/html/body/div[1]/div[3]/div/div/div[1]/div[1]/p")
+    car=cardets.text
+    fair_price=driver.find_element(By.XPATH,f"/html/body/div[1]/div[3]/div/div/div[1]/div[2]/div[2]/div[1]/p[2]")
+    fprice=fair_price.text
+    best_price=driver.find_element(By.XPATH,f"/html/body/div[1]/div[3]/div/div/div[1]/div[2]/div[2]/div[2]/p[2]")
+    bprice=best_price.text
+    extra=driver.find_element(By.XPATH,f"/html/body/div[1]/div[3]/div/div/div[1]/div[1]/div")
+    print(f"Your Car Evaluation:\nCar:{car}\nPrice Range:{fprice}-{bprice}")
+
+def selCar(cm, mm, km, fuel):
     time.sleep(5)
     
     loc = 1
@@ -49,55 +59,75 @@ def selCar(cm, mm, fuel, km):
             sl.click()
             time.sleep(3)
 
-            inp = driver.find_element(By.XPATH, f'/html/body/div[1]/div[{loc}]/div/div/div[1]/div/div[3]/div[1]/div[2]/div[2]/div[1]/div[2]/div/div/div[1]/div[2]/div/div/input') #dialog box
-            inp.click()
+            mkc = driver.find_element(By.XPATH, f'/html/body/div/div[3]/div/div/div[1]/div/div[3]/div[1]/div[2]/div[2]/div[2]/div/ul[1]') #dialog box maker list
+            mkclist = mkc.find_elements(By.TAG_NAME, "li")
+            y = 1
+            for j in mkclist:
+                y = y+1
+                makerName = j.find_element(By.XPATH, f"/html/body/div/div[3]/div/div/div[1]/div/div[3]/div[1]/div[2]/div[2]/div[2]/div/ul[1]/li[{y}]/div[1]/div[1]/span/span")
+                maker = makerName.text
+                print(maker.lower())
+                if cm.lower().count(maker.lower()):
+                    print("GREAT SUCCESS")
+                    print(y)
+                    makerName.click()
+                    break
 
             time.sleep(2)
-            inp.send_keys(f'{mm}')
+            print("carnow")
+            ckc = driver.find_element(By.XPATH, f"/html/body/div[1]/div[{loc}]/div/div/div[1]/div/div[3]/div[1]/div[2]/div[2]/div[2]/div/ul[1]/li[{y}]/div[2]/div/div/ul") #select
+            w = 1
+            ckclist = ckc.find_elements(By.TAG_NAME, "li")
+            for z in ckclist:
+                carName = z.find_element(By.XPATH, f"/html/body/div[1]/div[{loc}]/div/div/div[1]/div/div[3]/div[1]/div[2]/div[2]/div[2]/div/ul[1]/li[{y}]/div[2]/div/div/ul/li[{w}]/span/span")
+                w = w+1
+                car = carName.text
+                print(car)
+                print("hi")
+                if mm.lower().count(car.lower()):
+                    print("GREAT SUCCESS")
+                    carName.click()
+                    break
             time.sleep(5)
-            mkc = driver.find_element(By.XPATH, f"/html/body/div[1]/div[{loc}]/div/div/div[1]/div/div[3]/div[1]/div[2]/div[2]/div[2]/div/ul/li[2]/div/div/span/span/mark")
-            mkc.click()
-            time.sleep(5)
+
 
             ulst = driver.find_element(By.XPATH, f"/html/body/div[1]/div[{loc}]/div/div/div[1]/div/div[3]/div[1]/div[2]/div[2]/div[2]/div/ul")
             lilst = ulst.find_elements(By.TAG_NAME, "li")
-            print(lilst)         
             x = 1
             
             for i in lilst:    
-                txt = i.find_element(By.XPATH, f'/html/body/div[1]/div[{loc}]/div/div/div[1]/div/div[3]/div[1]/div[2]/div[2]/div[2]/div/ul/li[{x}]/div/div/p')
+                vrName = i.find_element(By.XPATH, f'/html/body/div[1]/div[{loc}]/div/div/div[1]/div/div[3]/div[1]/div[2]/div[2]/div[2]/div/ul/li[{x}]/div/div/p')
+                flt = i.find_element(By.XPATH, f'/html/body/div/div[3]/div/div/div[1]/div/div[3]/div[1]/div[2]/div[2]/div[2]/div/ul/li[{x}]/div/div/ul/li[1]').text
+                trns = i.find_element(By.XPATH, f'/html/body/div/div[3]/div/div/div[1]/div/div[3]/div[1]/div[2]/div[2]/div[2]/div/ul/li[{x}]/div/div/ul/li[2]').text
                 x = x+1
                 print(x)
-                a=txt.text
-                print(txt.text)
-                if a== fuel :
+                print(flt)
+                a=vrName.text
+                print(vrName.text)
+                if len(list(set(mm.upper().split()).intersection(a.upper().split()))) >= 2 and fuel == flt:
+                    
                     print("founddddd")
-                    lishit=driver.find_element(By.XPATH,f"/html/body/div/div[3]/div/div/div[1]/div/div[3]/div[1]/div[2]/div[2]/div[2]/div/ul/li[{x}]")
-                    lishit.click()
+                    vrName.click()
+                    break
                 else:
                     print("NOT FOUND")
-            print("break")
+
+            time.sleep(5)
             kms = driver.find_element(By.XPATH, f"/html/body/div[1]/div[{loc}]/div/div/div[1]/div/div[5]/div/div/input")
             kms.click()
             time.sleep(2)
             kms.send_keys(f"{km}")
             finalbutton=driver.find_element(By.XPATH,f"/html/body/div[1]/div[{loc}]/div/div/div[1]/div/div[6]/button")
-            finalbutton.click()  
+            finalbutton.click()
+                # 
+            
            
         except Exception as e:
             print(f"Error: {e}")
             loc = loc+1
 
            
-def getvalue():
-    cardets=driver.find_element(By.XPATH,f"/html/body/div[1]/div[3]/div/div/div[1]/div[1]/p")
-    car=cardets.text
-    fair_price=driver.find_element(By.XPATH,f"/html/body/div[1]/div[3]/div/div/div[1]/div[2]/div[2]/div[1]/p[2]")
-    fprice=fair_price.text
-    best_price=driver.find_element(By.XPATH,f"/html/body/div[1]/div[3]/div/div/div[1]/div[2]/div[2]/div[2]/p[2]")
-    bprice=best_price.text
-    extra=driver.find_element(By.XPATH,f"/html/body/div[1]/div[3]/div/div/div[1]/div[1]/div")
-    print(f"Your Car Evaluation:\nCar:{car}\nPrice Range:{fprice}-{bprice}")
+
         
 
 def getCwle(carMaker, makerModel, year, km, city, fuel):
@@ -114,11 +144,13 @@ def getCwle(carMaker, makerModel, year, km, city, fuel):
     fin_city = driver.find_element(By.XPATH, getPlc(city))
     fin_city.click()
 
-    selCar(carMaker, makerModel, fuel)
-
-
-    getvalue()
-
+    selCar(carMaker, makerModel, km, fuel)
     
-getCwle("", "wr-v", "2019", "35000", "mumbai", "Edge Edition Petrol [2018-2019]")
+getCwle("HYUNDAI MOTOR INDIA LTD", "CRETA 1.5 MPI MT SX", "2021", "35000", "mumbai", "Petrol")
+
+getvalue()
+
 driver.quit()
+
+
+#just determine transmission from model name and add it to if condition in selCar function 107th line then it will work fine
--- kadiuuuuuuuuu.py
+++ kadiuuuuuuuuu.py
@@ -1,8 +1,18 @@
-from gtts import gTTS
-from playsound import playsound
+# from gtts import gTTS
+# from playsound import playsound
 
-tts = gTTS(text='Kadiuuuuuuuuuuuuuuuuuuuuuuuuuuuuu', lang='en')
-tts.save('speech.mp3')
+# tts = gTTS(text='Kadiuuuuuuuuuuuuuuuuuuuuuuuuuuuuu', lang='en')
+# tts.save('speech.mp3')
 
-# play the speech using the default system player
-playsound('speech.mp3')
+# # play the speech using the default system player
+# playsound('speech.mp3')
+
+
+p = "CRETA 1.5 MPI MT SX"
+q = "SX 1.5 Petrol"
+print(p.lower().split().count(q.lower().split()))
+
+print(len(list(set(p.upper().split()).intersection(q.upper().split()))))
+
+#/html/body/div/div[3]/div/div/div[1]/div/div[3]/div[1]/div[2]/div[2]/div[2]/div/ul[1]/li[2]/div[2]/div/div/ul
+#/html/body/div[1]/div[{loc}]/div/div/div[1]/div/div[3]/div[1]/div[2]/div[2]/div[2]/div/ul[1]/li[2]/div[2]/div/div/ul
